ddb14318b26843624fa5fdb7081e90c2f4af42e6
Lgneous <jchedal-@e3r9p4.42.fr>
2019-04-02 18:05:53 +0200

    Remove unnecessary functions

--- src/tree_set.py
+++ src/tree_set.py
@@ -11,23 +11,8 @@ class TreeSet:
             bisect.insort(self._treeset, element)
             self._hashset.add(element)
 
-    def ceiling(self, e):
-        index = bisect.bisect_right(self._treeset, e)
-        if self[index - 1] == e:
-            return e
-        return self._treeset[bisect.bisect_right(self._treeset, e)]
-
-    def floor(self, e):
-        index = bisect.bisect_left(self._treeset, e)
-        if self[index] == e:
-            return e
-        return self._treeset[bisect.bisect_left(self._treeset, e) - 1]
-
     def __getitem__(self, i):
         return self._treeset[i]
-
-    def __len__(self):
-        return len(self._treeset)
 
     def remove(self, element):
         """
@@ -40,26 +25,8 @@ class TreeSet:
             return False
         return True
 
-    def __iter__(self):
-        """
-        Do ascending iteration for TreeSet
-        """
-        for element in self._treeset:
-            yield element
-
-    def pop(self, index):
-        self._hashset.pop(index)
-        return self._treeset.pop(index)
-
     def __str__(self):
         return str(self._hashset)
-
-    def __eq__(self, target):
-        if isinstance(target, TreeSet):
-            return self._treeset == target._treeset
-        if isinstance(target, list):
-            return self._treeset == target
-        return False
 
     def __contains__(self, e):
         """
