6d4c64e86e6adcad76396dfc3117e55761f4b000
Julien Chedal-Anglay <chedala.julien@gmail.com>
2019-04-06 07:52:08 +0200

    Add time complexity, and rename module

--- src/a_star.py
+++ src/a_star.py
@@ -6,23 +6,26 @@ def best_f_score(open_set):
     return min(open_set, key=lambda x: x.f_score)
 
 
-def a_star(heuristic, start):
+def run(start, heuristic):
     """A* algorithm
 
     Takes an heuristic function and an initial state as an input.
     Return the solution to the puzzle, or None if there is no solution
     """
-    open_set = {start}
-    closed_set = set()
+    start.apply_heuristic(heuristic)
 
     open_set = tree_set.TreeSet()
     closed_set = tree_set.TreeSet()
     open_set.add(start)
 
+    time_comp = 1
+    space_comp = 1
+
     while open_set:
         e = best_f_score(open_set)
         if e.done():
-            return e
+            return e, time_comp, space_comp
+        time_comp += 1
         open_set.remove(e)
         for s in e.expand():
             s.g_score = e.g_score + 1
@@ -30,40 +33,9 @@ def a_star(heuristic, start):
             if s in open_set or s in closed_set:
                 x = open_set.find_lt(s)
                 y = closed_set.find_lt(s)
-                if x and y:
-                    raise Exception("????????????????")
                 if x or y:
                     continue
             open_set.add(s)
+            space_comp = max(len(open_set), space_comp)
         closed_set.add(e)
-    return None
-
-
-import logic
-import heuristics
-
-import numpy as np
-
-
-grid = np.array([[2, 8, 3], [4, 0, 5], [1, 7, 6]])
-
-
-def display_path(e):
-    if e.parent is None:
-        return 0
-    x = 1 + display_path(e.parent)
-    print(e.parent)
-    return x
-
-
-s = logic.Puzzle(grid)
-e = a_star(heuristics.manhattan, s)
-print("Manhattan distance -")
-print("Total # of steps: {}".format(display_path(e)))
-
-print("\n{}\n".format("_" * 80))
-
-s = logic.Puzzle(grid)
-e = a_star(heuristics.hamming, s)
-print("Hamming distance -")
-print("Total # of steps: {}".format(display_path(e)))
+    return None, time_comp, space_comp
