0bc128d5c0dc1882bb3c1c94753029ed3f7d01aa
Julien Chedal-Anglay <chedala.julien@gmail.com>
2019-04-06 07:52:38 +0200

    Rename module, change some formatting and add memoization to expand

--- src/puzzle.py
+++ src/puzzle.py
@@ -2,17 +2,21 @@ import numpy as np
 
 
 class Puzzle:
-    def __init__(self, grid, parent=None):
+    def __init__(self, grid, parent=None, move="ORIGIN", goal=None):
         self.grid = np.array(grid)
         # required for hashing, also, mutability is never used
         self.grid.flags.writeable = False
         self.k = grid.shape[0]
         (self.y,), (self.x,) = np.where(grid == 0)
-        self._true_grid = np.array(Puzzle.make_goal(self.k))
-        self._true_grid = self._true_grid.reshape((self.k, self.k))
+        if goal is None:
+            self._goal = np.array(Puzzle.make_goal(self.k))
+            self._goal = self._goal.reshape((self.k, self.k))
+        else:
+            self._goal = goal
         self.h_score = 0
         self.g_score = 0
         self.parent = parent
+        self.move = move
 
     @staticmethod
     def make_goal(s):
@@ -45,7 +49,7 @@ class Puzzle:
         return puzzle
 
     def done(self):
-        return np.array_equal(self.grid, self._true_grid)
+        return np.array_equal(self.grid, self._goal)
 
     @property
     def f_score(self):
@@ -53,7 +57,7 @@ class Puzzle:
 
     def apply_heuristic(self, f):
         """Apply the heuristic function and set its value to _h_score"""
-        self.h_score = f(self.grid, self._true_grid, self.k)
+        self.h_score = f(self.grid, self._goal)
 
     def expand(self):
         """Yield all possible moves represented as copies of self with grid changed"""
@@ -61,35 +65,34 @@ class Puzzle:
             grid = self.grid.copy()
             grid[self.y, self.x] = grid[self.y - 1, self.x]
             grid[self.y - 1, self.x] = 0
-            yield Puzzle(grid, self)
+            yield Puzzle(grid, self, "UP", self._goal)
         if self.y + 1 < self.k:
             grid = self.grid.copy()
             grid[self.y, self.x] = grid[self.y + 1, self.x]
             grid[self.y + 1, self.x] = 0
-            yield Puzzle(grid, self)
+            yield Puzzle(grid, self, "DOWN", self._goal)
         if self.x - 1 >= 0:
             grid = self.grid.copy()
             grid[self.y][self.x] = grid[self.y, self.x - 1]
             grid[self.y][self.x - 1] = 0
-            yield Puzzle(grid, self)
+            yield Puzzle(grid, self, "LEFT", self._goal)
         if self.x + 1 < self.k:
             grid = self.grid.copy()
             grid[self.y, self.x] = grid[self.y, self.x + 1]
             grid[self.y, self.x + 1] = 0
-            yield Puzzle(grid, self)
+            yield Puzzle(grid, self, "RIGHT", self._goal)
 
     def __str__(self):
-        return "{} - {}".format(self.grid, self.f_score)
+        return "-- {} --\n{}".format(self.move, "\n".join(str(s) for s in self.grid))
 
     def __repr__(self):
         return str(self)
 
     def __eq__(self, other):
-        """Compare the 2 grids"""
         return np.array_equal(self.grid, other.grid)
 
     def __ne__(self, other):
-        return not (self == other)
+        return not self == other
 
     def __le__(self, other):
         return hash(self) <= hash(other)
@@ -103,6 +106,15 @@ class Puzzle:
     def __ge__(self, other):
         return hash(self) >= hash(other)
 
+    def __contains__(self, other):
+        if self == other:
+            return True
+        if self.parent is None:
+            return False
+        return other in self.parent
+
     def __hash__(self):
-        """Complex hash for set usage, f-score added for ordering in tree with comparison functions"""
+        """Complex hash for set usage, f-score added for ordering in tree with
+        comparison function
+        """
         return hash(self.grid.tostring()) + self.f_score
