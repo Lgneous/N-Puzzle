5971dca5cb1164b9b9f440b1c22c10a5e8d24406
Lgneous <jchedal-@e3r7p12.42.fr>
2019-04-10 10:42:52 +0200

    Lambda to def

--- src/ucs.py
+++ src/ucs.py
@@ -2,5 +2,6 @@ import a_star
 
 
 def run(start, _, greedy=False):
-    heuristic = lambda *args, **kwargs: 0
+    def heuristic(*_, **__):
+        return 0
     return a_star.run(start, heuristic, greedy)
